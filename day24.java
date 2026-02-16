2aede8885f13abaab080264950c6beb107be7b3c
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-02-16 21:19:40 +0530

    Implement borrowing calculation

---
+++ day24.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+
+        int totalCost = k * w * (w + 1) / 2;
+
+        int borrow = Math.max(0, totalCost - n);
+        System.out.println(borrow);
+    }
+}
